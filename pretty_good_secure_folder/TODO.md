6da9fa357655ce155baab74dd78008ef09e37efc
Kaito Kitaya <kaito.kitaya.deutschland@gmail.com>
2025-11-21 15:34:50 +0100

    add: TODO

---
+++ pretty_good_secure_folder/TODO.md
@@ -0,0 +1,9 @@
+# Priority high
+- [ ] Password
+- [ ] Holder Sort
+- [ ] Favorite
+- [ ] PGP
+- [ ] Holder Copy
+
+# Priority middle
+- [ ] Template
--- pretty_good_secure_folder/lib/view/main/main_view.dart
+++ pretty_good_secure_folder/lib/view/main/main_view.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
-import 'package:pretty_good_secure_folder/model/error/app_error.dart';
 import 'package:pretty_good_secure_folder/view/main/slide_item_view.dart';
 
 class MainView extends HookConsumerWidget {
@@ -11,7 +9,7 @@ class MainView extends HookConsumerWidget {
   Widget build(BuildContext context, WidgetRef ref) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Pretty Good Secure Folder'),
+        title: const Text('Your Folder'),
       ),
       body: SlideItemView()
     );
